d1887f5f83f1f1582017275a111693f1ea5ea47e
Ahammed Fahim Galib <galib0124gmail.com>
2023-02-02 18:28:44 +0600

    banner section complete

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,7 +5,8 @@ const Banner = () => {
     return (
         <div className='banner'>
             <div className='w-75 mx-auto'>
-                <h1>This is text</h1>
+                <h1 className='heoro-text'>Computer Engineering</h1>
+                <p className='text-white fs-5 fs-sm-6'>142,765 Computer Engineers follow this</p>
             </div>
         </div>
     );
